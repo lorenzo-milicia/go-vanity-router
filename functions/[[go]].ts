be8b9f320163a705fdcce56de22d2ff10cec3544
Lorenzo Milicia <info@lorenzomilicia.dev>
2023-03-11 12:50:21 +0100

    Initial commit

---
+++ functions/[[go]].ts
@@ -0,0 +1,67 @@
+interface Env {
+    CUSTOM_DOMAIN: string;
+    REPOSITORY_URL: string;
+    CUSTOM_ROUTINGS: string;
+}
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+    let response = new Response(buildHtml(context))
+    response.headers.set("Content-Type", "text/html")
+    return response
+}
+
+function buildHtml(context: EventContext<Env, any, Record<string, unknown>>): string {
+    return `
+	<!doctype html>
+	<html>
+	<head>
+	${buildMetaTag(context)}
+	${buildRoutingTag(context)}
+	</head>
+	<body>
+	<div>
+	<p>Check the meta tag</p>
+	</div>
+	</body>
+	</html>
+	`.trim()
+}
+
+function buildMetaTag(context: EventContext<Env, any, Record<string, unknown>>): string {
+    let vanityPath = context.functionPath
+    let customDomain: string = context.env.CUSTOM_DOMAIN
+    let repositoryUrl: string = context.env.REPOSITORY_URL
+    let customRoutings = getCustomRoutingsFrom(context.env.CUSTOM_ROUTINGS)
+    let repositoryPath = routeVanityPath(vanityPath, customRoutings)
+
+    return `<meta name="go-import" content="https://${customDomain}${vanityPath} git https://${repositoryUrl}${repositoryPath}">`
+}
+
+function buildRoutingTag(context: EventContext<Env, any, Record<string, unknown>>): string {
+    let vanityPath = context.functionPath
+    if (vanityPath == "/") return ""
+    let repositoryUrl: string = context.env.REPOSITORY_URL
+    let customRoutings = getCustomRoutingsFrom(context.env.CUSTOM_ROUTINGS)
+    let repositoryPath = routeVanityPath(vanityPath, customRoutings)
+
+    return `<meta http-equiv="Refresh" content="0; url='https://${repositoryUrl}${repositoryPath}'"/>`
+}
+
+function getCustomRoutingsFrom(routingsEnv: string): Map<string, string> {
+    let splitString = routingsEnv.split(",")
+    let map = new Map()
+    for (let i = 0; i < splitString.length / 2; i++) {
+        map.set(splitString[i], splitString[i + 1])
+    }
+    return map
+}
+
+
+function routeVanityPath(vanityPath: string, customRoutings: Map<string, string>): string {
+    let repositoryPath = vanityPath
+    customRoutings.forEach((value, key) => {
+            repositoryPath = repositoryPath.replace(key, value)
+        }
+    )
+    return repositoryPath
+}
